333332132ebfc2e44e3f596d8667b6e508a275a4
hkt.zaradev <hkt.zaradev@gmail.com>
2021-02-12 20:19:52 +1300

    Added tables for the course selection page

---
+++ src/components/CourseTest.jsx
@@ -0,0 +1,60 @@
+/**
+ * Used to test the layout and the functions of the website.
+ * Will replace it with an API when ready.
+ * 
+ * Time References:
+ *      Hillcrest Rd
+ *          Day Classes:
+ *              11:00am - 11:50am
+ *              1:10pm - 2:00pm
+ *          Evening Classes:
+ *              6:00pm - 7:00pm
+ * 
+ *      Ward St
+ *          Day Classes:
+ *              12:10pm - 1:00pm
+ *          Evening Classes:
+ *              6:30pm - 7:30pm
+ *              6:30pm - 8:10pm
+ */
+export const courseTest = [
+    {
+        id: 1,
+        title: "Christ and the Everlasting Gospel",
+        location: "Ward St",
+        courseType: "Cornerstone Course",
+        teacher: "Brother Skux",
+        timeOfDay: "day",
+        dayOfWeek: "Wednesday",
+        startTime: "1:10",
+        thumbnailURL: "https://assets.ldscdn.org/c9/e3/c9e3fede4d3d3f40d5d116264f40cfea1badfd6f/jesus_last_supper.jpeg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",    
+        url:"/christ-and-the-everlasting-gospel"
+    },
+    {
+        id: 2,
+        title: "Book of Mormon Book Club",
+        location: "Hillcrest Rd",
+        courseType: "Primary Elective",
+        teacher: "Sister Stud",
+        timeOfDay: "day",
+        dayOfWeek: "Tuesday",
+        startTime: "6:30",
+        thumbnailURL: "https://assets.ldscdn.org/2d/9f/2d9f8689e00d4b026e9f68f531605176634e7dba/brass_plates_book_of_mormon_visual_library.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",    
+        url:"/bom-book-club"
+    },
+    {
+        id: 3,
+        title: "Missionary Preparation",
+        location: "Hillcrest Rd",
+        courseType: "Secondary Elective",
+        teacher: "Brother Ethan Watene",
+        timeOfDay: "evening",
+        dayOfWeek: "Tuesday",
+        startTime: "6:30",
+        thumbnailURL: "https://assets.ldscdn.org/2d/9f/2d9f8689e00d4b026e9f68f531605176634e7dba/brass_plates_book_of_mormon_visual_library.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",    
+        url:"/bom-book-club"
+    }
+];
--- src/components/routes/Selection.js
+++ src/components/routes/Selection.js
@@ -3,42 +3,9 @@ import "../styles/Selection.css";
 import CourseModal  from "../CourseViews";
 import GoogleMap from '../map/GoogleMap';
 import { Switch, Router } from 'react-router-dom';
-import { Tab, Header } from 'semantic-ui-react';
+import { Tab, Header, Table } from 'semantic-ui-react';
 import ReactDOM from 'react-dom';
-
-/***************************************************************************************/
-
-/**
- * Used to test the layout and the functions of the website.
- * Will replace it with an API when ready.
- */
-const courseTest = [
-    {
-        id: 1,
-        title: "Christ and the Everlasting Gospel",
-        location: "Ward St",
-        courseType: "Cornerstone Course",
-        teacher: "Brother Skux",
-        day: "Wednesday",
-        startTime: "",
-        thumbnailURL: "https://assets.ldscdn.org/c9/e3/c9e3fede4d3d3f40d5d116264f40cfea1badfd6f/jesus_last_supper.jpeg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",    
-        url:"/christ-and-the-everlasting-gospel"
-    },
-    {
-        id: 2,
-        title: "Book of Mormon Book Club",
-        location: "Hillcrest Rd",
-        courseType: "Primary Elective",
-        teacher: "Sister Stud",
-       
-        thumbnailURL: "https://assets.ldscdn.org/2d/9f/2d9f8689e00d4b026e9f68f531605176634e7dba/brass_plates_book_of_mormon_visual_library.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",    
-        url:"/bom-book-club"
-    }
-];
-
-/***************************************************************************************/
+import { courseTest } from '../CourseTest';
 
 
 /**
@@ -121,10 +88,90 @@ export default class Selection extends React.Component {
                     render: () => <Tab.Pane attached={false}>
                         {/* <GoogleMap locationIndex={0}/>                    */}
                         <Header className="tab-header" as="h2">Day Classes</Header>
-                        <hr/>
-                        <CourseModal courses={this.hillcrestCourses}/>
+                        
+                        <div className="table-container">
+                            <Table
+                                stackable={false}
+                                color="red" 
+                                celled 
+                                padded 
+                                style={{fontFamily: "'verdana'"}}
+                            >
+                                <Table.Header>
+                                    <Table.Row>
+                                        <Table.HeaderCell textAlign="center">Time</Table.HeaderCell>
+                                        <Table.HeaderCell textAlign="center">Monday</Table.HeaderCell>
+                                        <Table.HeaderCell textAlign="center">Tuesday</Table.HeaderCell>
+                                        <Table.HeaderCell textAlign="center">Wednesday</Table.HeaderCell>
+                                        <Table.HeaderCell textAlign="center">Thursday</Table.HeaderCell>
+                                        <Table.HeaderCell textAlign="center">Friday</Table.HeaderCell>
+                                    </Table.Row>
+                                </Table.Header>
+                                <Table.Body>
+                                    {/* classes from 11:10am - 11:50am at Hillcrest Rd */}
+                                    <Table.Row>
+                                        <Table.HeaderCell textAlign="center">11:10 am — 11:50 am</Table.HeaderCell>
+                                        <Table.Cell textAlign="center">
+                                            <div>
+                                                <CourseModal courses={this.hillcrestCourses}/>
+                                            </div>
+                                        </Table.Cell>
+                                        <Table.Cell textAlign="center">
+                                            <div>
+                                                <CourseModal courses={this.hillcrestCourses}/>
+                                            </div>
+                                        </Table.Cell>
+                                        <Table.Cell textAlign="center"></Table.Cell>
+                                        <Table.Cell textAlign="center"></Table.Cell>
+                                        <Table.Cell textAlign="center"></Table.Cell>
+                                    </Table.Row>
+                                    
+                                    {/* classes from 1:10pm - 2:00pm at Hillcrest Rd */}
+                                    <Table.Row>
+                                        <Table.HeaderCell textAlign="center">1:10 pm — 2:00 pm</Table.HeaderCell>
+                                        <Table.Cell textAlign="center"></Table.Cell>
+                                        <Table.Cell textAlign="center">
+                                        <div>
+                                                <CourseModal courses={this.hillcrestCourses}/>
+                                            </div>
+                                        </Table.Cell>
+                                        <Table.Cell textAlign="center"></Table.Cell>
+                                        <Table.Cell textAlign="center"></Table.Cell>
+                                        <Table.Cell textAlign="center">
+                                            <div>
+                                                <CourseModal courses={this.hillcrestCourses}/>
+                                            </div>
+                                        </Table.Cell>
+                                    </Table.Row>
+                                </Table.Body>
+                            </Table>
+                        </div>
+            
+                        
                         <Header className="tab-header" as="h2">Evening Classes</Header>
-                        <hr/>
+                        <Table color="violet" celled padded style={{overflowX: "auto"}}>
+                            <Table.Header>
+                                    <Table.Row>
+                                        <Table.HeaderCell>Time</Table.HeaderCell>
+                                        <Table.HeaderCell>Monday</Table.HeaderCell>
+                                        <Table.HeaderCell>Tuesday</Table.HeaderCell>
+                                        <Table.HeaderCell>Wednesday</Table.HeaderCell>
+                                        <Table.HeaderCell>Thursday</Table.HeaderCell>
+                                        <Table.HeaderCell>Friday</Table.HeaderCell>
+                                    </Table.Row>
+                                </Table.Header>
+                                <Table.Body>
+                                    {/* classes from 6:00 pm - 7:00 pm at Hillcrest Rd */}
+                                    <Table.Row>
+                                        <Table.HeaderCell>6:00 pm — 7:00 pm</Table.HeaderCell>
+                                        <Table.Cell></Table.Cell>
+                                        <Table.Cell></Table.Cell>
+                                        <Table.Cell></Table.Cell>
+                                        <Table.Cell></Table.Cell>
+                                        <Table.Cell></Table.Cell>
+                                    </Table.Row>
+                                </Table.Body>
+                            </Table>
                     </Tab.Pane>,
                 },
                 {
@@ -151,9 +198,9 @@ export default class Selection extends React.Component {
             <div className="selection">
                 {this.props.header}
                 
-                <Tab panes={panes}/>
+                <Tab className="selection-tabs" panes={panes}/>
         
-                {this.props.footer}   
+                {/*this.props.footer*/}   
             </div>
         );
     };
