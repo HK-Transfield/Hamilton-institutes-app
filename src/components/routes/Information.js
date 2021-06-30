765f7543d76de204cbea1bdd5ac81b01a67c4755
hkt.zaradev <hkt.zaradev@gmail.com>
2021-07-01 09:36:07 +1200

    Changed to a functional component and improved comments.

--- src/components/routes/Information.js
+++ src/components/routes/Information.js
@@ -1,13 +1,23 @@
 import React from "react";
 
-export default class Information extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.header}
-                <p>Hello there</p>
-                {this.props.footer}
-            </div>
-        );
-    };
-};
+/**
+ * This page will eventually display more detailed information
+ * about what Institute is, quotes from leaders of the church
+ * and a welcome from the institute directors.
+ * 
+ * @param {Element} props.header
+ * @param {Element} props.footer 
+ * @returns The Information component node
+ * 
+ * @author HK Transfield
+ */
+const Information = ({header, footer}) => {
+    return(
+        <div>
+            {header}
+            <p>Hello there, this page will eventually get updated.</p>
+            {footer}
+        </div>
+    );
+}
+export default Information;
